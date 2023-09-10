86b635df8e5f093e1c6654fb259c81cce7e98b43
Petridi <tarcev.vv@gmail.com>
2023-09-11 01:32:28 +0300

    figure

---
+++ Figure_v1/Figure_v1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Figure {
+protected:
+    int sides;
+    string name;
+public:
+    Figure() {
+        sides = 0;
+        name = "Фигура:";
+    }
+    int getNumberOfSides() {
+        return sides;
+    }
+    string getNameOfSides() {
+        return name;
+    }
+};
+
+class Triangle : public Figure {
+public:
+    Triangle() {
+        sides = 3;
+        name = "Треугольник:";
+    }
+};
+
+class Quadrilateral : public Figure {
+public:
+    Quadrilateral() {
+        sides = 4;
+        name = "Четырехугольник:";
+    }
+};
+
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+
+    Figure figure;
+    Triangle triangle;
+    Quadrilateral quadrilateral;
+
+    cout << "Количество сторон:" << endl;
+    cout << figure.getNameOfSides() << " " << figure.getNumberOfSides() << endl;
+    cout << triangle.getNameOfSides() << " " << triangle.getNumberOfSides() << endl;
+    cout << quadrilateral.getNameOfSides() << " " << quadrilateral.getNumberOfSides() << endl;
+
+    return 0;
+}
